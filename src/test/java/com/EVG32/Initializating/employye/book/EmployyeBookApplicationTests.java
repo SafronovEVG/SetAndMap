b651cadce1c69445ef2f75eac72e2e3263410015
Евгений <Safronovbr@mail.ru>
2024-11-04 14:25:26 +0300

    start

---
+++ src/test/java/com/EVG32/Initializating/employye/book/EmployyeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EVG32.Initializating.employye.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployyeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
